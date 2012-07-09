10c336eac95a8d9ec04dc5a32fb41735435ae7a5
ant30 <ant30tx@gmail.com>
2012-07-09 13:32:53 +0200

    Adding dhcpd backend

---
+++ ninjasysop/backends.py
@@ -0,0 +1,62 @@
+import pkg_resources
+
+ENTRYPOINT = 'ninjasysop.plugins'
+
+
+class BackendApplyChangesException(Exception):
+    pass
+
+
+class Backend(object):
+
+    _shared_states = {}
+    def __init__(self, name, filename):
+        self.__dict__ = self._shared_states
+        self.name = name
+        self.filename = filename
+
+    def get_name(self):
+        return self.name
+
+    def get_filename(self):
+        return self.filename
+
+    def del_item(self, name):
+        raise NotImplementedError("Not del_item implemented")
+
+    def get_item(self, name):
+        raise NotImplementedError("Not get_item implemented")
+
+    def get_items(self, **kwargs):
+        raise NotImplementedError("Not get_items implemented")
+
+    def add_item(self, **kwargs):
+        raise NotImplementedError("Not add_item implemented")
+
+    def save_item(self, **kwargs):
+        raise NotImplementedError("Not save_item implemented")
+
+    def get_edit_schema(self):
+        raise NotImplementedError("Not get_editform implemented")
+
+    def get_add_schema(self, name):
+        raise NotImplementedError("Not get_addform implemented")
+
+    def apply_changes(self):
+        raise NotImplementedError("Not apply_changes implemented")
+
+    @classmethod
+    def get_texts(self):
+        raise NotImplementedError("Not get_texts implemented")
+
+    @classmethod
+    def __unicode__(self):
+        return self.__name__
+
+
+def load_backends():
+    Backends = {}
+    for entrypoint in pkg_resources.iter_entry_points(ENTRYPOINT):
+        backend_class = entrypoint.load()
+        Backends[backend_class.__name__] = backend_class
+    return Backends
---
+++ plugins/dhcpd/dhcpd.py
@@ -0,0 +1,189 @@
+import re
+import subprocess
+
+from ninjasysop.backends import Backend, BackendApplyChangesException
+from ninjasysop.validators import IntegrityException
+
+from texts import texts
+import ipdb; ipdb.set_trace()
+from forms import AddHostForm, EditHostForm, DhcpHostValidator
+
+# SERIAL = yyyymmddnn ; serial
+PARSER_RE = {
+    'partition':re.compile(r"(?P<sub>subnet[^\}]*}) *(?P<hosts>.*)$"),
+    'hosts': re.compile(r"host (?P<hostname>[^ ]*) *{ *hardware ethernet (?P<mac>[^\;]*); *fixed-address (?P<ip>[^\;]*)")
+}
+
+MATCH_RE_STR = {
+    'record':r'^{name} *(?:\d+ *|)(?:IN *|){rtype}',
+}
+
+RELOAD_COMMAND = "/etc/init.d/isc-dhcpd-server reload"
+
+
+
+class DhcpdHost(object):
+    def __init__(self, name, mac, ip, comment=''):
+        self.ip = ip
+        self.mac = mac
+        self.name = name
+        self.comment = comment or ''
+
+    def __str__(self):
+        return self.name
+
+    def todict(self):
+        return dict(name = self.name,
+                    mac = self.mac,
+                    ip = self.ip,
+                    comment = self.comment)
+
+
+class NetworkFile(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def readfile(self):
+        serial = ''
+        items = {}
+        with open(self.filename, 'r') as networkfile:
+            content = networkfile.read()
+            partition = PARSER_RE['partition'].search(content.replace("\n",""))
+            if not partition:
+                raise IOError("Bad File Format")
+            (header, hosts) = partition.groups()
+            parsed_hosts = PARSER_RE['hosts'].findall(hosts)
+            for (name, mac, ip) in parsed_hosts:
+                item = Item(name, mac, ip)
+                items[name] = item
+
+        return items
+
+    def __str_item(self, item):
+        itemstr = ''
+        if item.comment:
+            itemstr = "#{0}\n".format(item.comment)
+
+        itemstr += "host {name} {{\n hardware ethernet {mac};\n fixed-address {ip};\n}}\n".format(
+                                    name=item.name, mac=item.mac, ip=item.ip)
+
+        return itemstr
+
+
+    def add_item(self, item):
+
+        with open(self.filename, 'r') as filecontent:
+
+            item_str = self.__str_item(item)
+            content = filecontent.read()
+            content = content + item_str
+
+        with open(self.filename, 'w') as filecontent:
+            filecontent.write(content)
+
+
+    def save_item(self, old_item, item):
+
+        with open(self.filename, 'r') as filecontent:
+
+            item_str = self.__str_item(item)
+            content = filecontent.read()
+            content_1 = re.sub(r"host %s [^\{]*{.*[^\}]*} *\n" % (old_item.name),
+                    item_str, content)
+            if content == content_1:
+                raise KeyError("host %s not found" % item.name)
+            else:
+                content = content_1
+
+        with open(self.filename, 'w') as filecontent:
+            filecontent.write(content)
+
+    def remove_item(self, item):
+        with open(self.filename, 'r') as filecontent:
+
+            content = filecontent.read()
+            content_1 = re.sub(r"host %s [^\{]*{.*[^\}]*} *\n" % (item.name),
+                               "", content)
+            if content == content_1:
+                raise KeyError("host %s not found" % item.name)
+            else:
+                content = content_1
+
+        with open(self.filename, 'w') as filecontent:
+            filecontent.write(content)
+
+
+class Dhcpd(Backend):
+
+    def __init__(self, name, filename):
+        super(Dhcpd, self).__init__(name, filename)
+        self.networkfile = Networkfile(filename)
+        self.items = self.networkfile.readfile()
+
+    def del_item(self, name):
+        self.networkfile.remove_item(self.items[name])
+        del self.items[name]
+
+    def get_item(self, name):
+        if name in self.items:
+            return self.items[name]
+        else:
+            return None
+
+    def get_items(self, **kwargs):
+        filters = []
+
+        if 'name' in kwargs:
+            def filter_name(r):
+                return r.name.find(kwargs['name']) >= 0
+            filters.append(filter_name)
+
+        if 'mac' in kwargs:
+            def filter_mac(r):
+                return r.mac == kwargs['mac']
+            filters.append(filter_type)
+
+        if 'ip' in kwargs:
+            def filter_target(r):
+                return r.ip == ip
+            filters.append(filter_target)
+
+        if 'name_exact' in kwargs:
+            def filter_name_exact(r):
+                return r.name == kwargs['name']
+            filters.append(filter_name_exact)
+
+        if filters:
+            return filter(lambda item: all([f(item) for f in filters]),
+                         self.items.values())
+        else:
+            return self.items.values()
+
+
+    def add_item(self, **kwargs):
+        item = Item(name=name,
+                    mac=mac,
+                    ip=ip,
+                    comment=comment)
+
+        self.networkfile.add_item(item)
+        self.items[str(item)] = item
+
+    def save_item(self, **kwargs):
+        item = Item(name=name,
+                    mac=mac,
+                    ip=ip,
+                    comment=comment)
+
+        self.networkfile.save_item(old_item, item)
+        self.items[str(old_item)] = item
+
+    def get_edit_schema(self, name):
+        return DhcpEditHostForm(validator=DhcpHostValidator(self))
+
+    def get_add_schema(self):
+        return DhcpAddHostForm(validator=DhcpHostValidator(self))
+
+    @classmethod
+    def get_texts(self):
+        return texts
